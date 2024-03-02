cc6ab52fed5f1ce2339758d1a5f9d207d8629698
rghouzra <rghouzra@student.1337.ma>
2024-03-02 19:52:42 +0100

    ex02 done

--- cpp_module07/ex02/Array.hpp
+++ cpp_module07/ex02/Array.hpp
@@ -7,11 +7,63 @@
 #include <iostream>
 
 
-template<typename t>
+template<typename T>
 class Array
 {
 public:
-    
+	const T *getP2elements() const{
+		return p;
+	}
+	unsigned int size() const{
+		return __size;
+	}
+	Array()
+	{
+		p = NULL;
+		__size = 0;
+	}
+	Array(unsigned int n)
+	{
+		__size  = n;
+		p = new T[n];
+		for(unsigned int i = 0; i < __size;i++)
+			p[i] = i;
+	}
+	Array(const Array &obj)
+	{
+		if(this != &obj){
+			unsigned int _size = obj.size();
+			p = new T[_size];
+			__size = _size;
+			for(unsigned int i = 0; i < _size ; i++)
+				this->p[i] = obj.p[i];
+		}
+	}
+
+	Array &operator=(const Array &obj){
+		if(this != &obj){
+			unsigned int _size = obj.size();
+			delete []this->p;
+			this->p = new T[_size];
+			__size = _size;
+			for (unsigned int i = 0; i < _size; i++)
+			{
+				this->p[i] = obj.p[i];
+			}
+		}
+		return *this;
+	}
+	T &operator[](unsigned int index){
+		if(index < __size){
+			return this->p[index];
+		}
+		throw std::runtime_error("out of bounds");
+	}
+	~Array(){
+		delete []p;
+	}
 private:
+	T *p;
+	unsigned int __size;
 };
 #endif /*Array_HPP*/
--- cpp_module07/ex02/main.cpp
+++ cpp_module07/ex02/main.cpp
@@ -3,8 +3,42 @@
 //
 #include "Array.hpp"
 
+
+void checker(){
+    system("leaks Array");
+}
+
 int main()
 {
     // Your Code
+    atexit(checker);
+    try{
+        int size = 5;
+        int size2 = size + 11;
+        Array<int> p(size);
+        Array<int> p2(p);
+        Array<int> p3(size2);
+
+
+        const int *ptr =p2.getP2elements();
+
+
+        for(int i = 0; i < size; i++){
+        std::cout << ptr[i] << '\n';
+        }
+        p2 = p3;
+        const int *ptr1 =p2.getP2elements();
+
+        for(int i = 0; i < size2; i++){
+        std::cout << ptr1[i] << '\n';
+        }
+        std::cout << "------------\n";
+        for(int i = 0; i < size2 + 1; i++){
+        std::cout << p2[i] << '\n';
+        }
+    }
+    catch(std::exception &e){
+        std::cerr << e.what() << '\n';
+    }
     return 0;
 }

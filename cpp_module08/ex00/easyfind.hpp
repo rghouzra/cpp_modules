e77b63acf41a5eaff8c239409614616550c66b98
rghouzra <rghouzra@student.1337.ma>
2024-03-02 22:53:02 +0100

    done

---
+++ cpp_module08/ex00/easyfind.hpp
@@ -0,0 +1,11 @@
+//
+// Created by reda ghouzraf on Sat Mar  2 20:53:10 2024
+//
+#ifndef easyfind_HPP
+#define easyfind_HPP
+
+#include <iostream>
+
+template<typename T, typename >
+
+#endif /*easyfind_HPP*/
---
+++ cpp_module08/ex00/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by reda ghouzraf on Sat Mar  2 20:53:10 2024
+//
+#include "easyfind.hpp"
+int main()
+{
+
+
+    return 0;
+}
